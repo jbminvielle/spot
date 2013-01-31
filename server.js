d4bb960b0d7ced32df1410bead9af8f6febf606e
Jean-Baptiste Minvielle <jean.baptiste.minvielle@gmail.com>
2013-01-31 12:37:09 +0100

    tests node.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var url = require("url");
+
+function start(route) {
+  function onRequest(request, response) {
+  	var pathname = url.parse(request.url).pathname;
+
+    console.log("Requête reçue pour le chemin " + pathname + ".");
+
+    route(pathname);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Requête executée");
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log("Démarrage du serveur.");
+}
+
+exports.start = start;
